1bb4babe9e089dca1004bf505d86b5e62854a746
luzac3 <rey.silvy@gmail.com>
2021-06-17 17:37:33 +0900

    共通処理実装

---
+++ common/php/conection.php
@@ -0,0 +1,42 @@
+<?php
+
+function db_connect(){
+    $dbInfo = new DbInfo;
+
+    $server = $dbInfo->server;
+    $username = $dbInfo->username;
+    $password = $dbInfo->password;
+    $db_name = $dbInfo->db_name;
+
+    $mysqli = new mysqli($server,$username,$password,$db_name);
+    if($mysqli->connect_error){
+        echo $mysqli->connect_error;
+        exit();
+    }else{
+        $mysqli->set_charset("utf8");
+    }
+    return $mysqli;
+}
+
+class DbInfo{
+    public $server = "";
+    public $username = "";
+    public $password = "";
+    public $db_name = "";
+    public $host = "";
+
+    function __construct(){
+        if ($_SERVER['HTTP_HOST'] == "localhost"){
+            $this->server = "localhost";
+            $this->username = "root";
+            $this->password = "alderaan";
+            $this->db_name = "sparrow";
+        }else{
+            $this->server = "mysql622.db.sakura.ne.jp";
+            $this->username = "wolfnet-twei";
+            $this->password = "alderaan123";
+            $this->db_name = "wolfnet-twei_sparrow";
+        }
+    }
+}
+?>

1b16accd4acf78d2d7e7fbd01261a42cecc471e7
osugi <oosugi-t@vehiclevision.jp>
2021-12-06 13:31:35 +0900

    defaultAjaxTS化

---
+++ typescripts/common/defaultAjax.ts
@@ -0,0 +1,41 @@
+export class DefaultAjax {
+  argArr: [];
+  phpAddr: string;
+  timeOut: number;
+
+  constructor(argArr: [], phpAddr: string, timeOut:number = 1000) {
+    this.argArr = argArr;
+    this.phpAddr = phpAddr;
+    this.timeOut = timeOut;
+  }
+
+  post(){
+    return new Promise((resolve: (value?: string) => void, reject: (reason?: any) => void) => {
+        $.ajax({
+            url: this.phpAddr
+            ,cache: false
+            // ,timeout: time_out
+            ,type:'POST'
+            ,dataType: 'json'
+            ,data:{
+                argArr: this.argArr
+            }
+            //,processData: false
+            //,contentType: false
+            //,traditional: true
+        }).then(
+            function(data: any){
+                console.log(data);
+                resolve(data);
+            },
+            function(jqXHR: JQueryXHR, textStatus: string, errorThrown: string){
+                console.log("更新処理に失敗しました");
+                console.log("jqXHR : " + jqXHR.status);
+                console.log("textStatus     : " + textStatus);
+                console.log("errorThrown    : " + errorThrown);
+                reject(0);
+            }
+        );
+    });
+  }
+}

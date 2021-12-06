80d8ed3e5976bb4cb9b46f9f6658756b38c64b2d
osugi <oosugi-t@vehiclevision.jp>
2021-12-06 13:32:58 +0900

    storger修正

--- typescripts/common/storager.ts
+++ typescripts/common/storager.ts
@@ -1,24 +1,19 @@
 	export class storager{
-		private static convertFunctionsToStringsInJSON(json: Object){
-	    for (const key in json) {
-	      if(typeof(json[key]) === "function") {
-		      json[key] = json[key].toString();
-		    }
-	    }
-
-    	return json;
-		}
-
 		static set(Obj_name: string,Obj: Object){
 			window.sessionStorage.setItem(Obj_name, JSON.stringify(Obj));
 		}
 
-		static get(Obj_name: string){
-			const obj = window.sessionStorage.getItem(Obj_name);
-			const toStringObj = this.convertFunctionsToStringsInJSON(obj);
+		static get(objName: string){
+			const obj = window.sessionStorage.getItem(objName);
+			if(!this.check(objName)){
+				return false;
+			}
+			if(obj === null){
+				throw new Error("未知のエラー")
+			}
 			const parser = function(k: string,v: Object){return v.toString().indexOf('function') === 0 ? eval('('+v+')') : v};
 
-			return JSON.parse(JSON.stringify(toStringObj), parser);
+			return JSON.parse(obj, parser);
 		}
 
 		static check(Obj_name: string){

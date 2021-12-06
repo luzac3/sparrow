d935db27b5f82cac541faa5a33ac8189b73048be
osugi <oosugi-t@vehiclevision.jp>
2021-12-06 14:34:54 +0900

    registerTS化

---
+++ typescripts/pages/register.ts
@@ -0,0 +1,39 @@
+import {CallStored} from '../common/callStored';
+import {SearchCookie} from '../common/searchCookie';
+
+$(document).ready(() => {
+  const callStored = new CallStored();
+  const searchCookie = new SearchCookie();
+  const userNum = searchCookie.getCookie('user_num');
+
+  if(!userNum){
+    alert("ユーザー番号がありません");
+    return;
+  }
+
+  $(".yes").on("click", () => {
+    // ajaxで処理を飛ばす
+    callStored.callSql(
+      {
+        user_num: userNum
+        ,user_name: String($("#inputName").val())
+      }
+      ,"registerUserName"
+    ).then((data: any) => {
+      console.log(data);
+
+      if(data["exit_cd"] == 0){
+
+        location.href = "/sparrow/front/home.html";
+      }
+    }, () => {
+      console.log("err");
+    }).catch(
+      err => alert(err)
+    );
+  });
+
+  $(".no").on("click", () => {
+    location.reload();
+  });
+});

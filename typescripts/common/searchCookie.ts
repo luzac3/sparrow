30a2ea8a93f1d048ba85deb80a2f028a40d66543
osugi <oosugi-t@vehiclevision.jp>
2021-12-06 09:33:56 +0900

    searchCookieTS化

---
+++ typescripts/common/searchCookie.ts
@@ -0,0 +1,22 @@
+export class SearchCookie {
+  cookiesArray = [];
+
+  constructor(){
+    const cookies = document.cookie;
+    this.cookiesArray = cookies.split(';');
+  }
+
+  getCookie(key: string) {
+    let cArray = [];
+
+    for(let c of this.cookiesArray){
+      cArray = c.split('='); //さらに=で分割して配列に
+      if( cArray[0].trim() === key){ // 取り出したいkeyと合致したら
+          return cArray[1].trim();  // [key,value]
+      }
+    }
+
+    // マッチしなかった場合
+    return false;
+  }
+}

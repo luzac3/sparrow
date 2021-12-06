afce57b07ff0344795b54b91a18151478e5c6fee
osugi <oosugi-t@vehiclevision.jp>
2021-12-06 13:39:14 +0900

    callStoredTS化

---
+++ typescripts/common/callStored.ts
@@ -0,0 +1,42 @@
+export class CallStored {
+  argArr: [];
+  storedName: string;
+  timeOut: number;
+
+  constructor(argArr: [], storedName: string, timeOut:number = 1000) {
+    this.argArr = argArr;
+    this.timeOut = timeOut;
+    this.storedName = storedName;
+  }
+
+  callSql(){
+    return new Promise((resolve: (value?: string) => void, reject: (reason?: any) => void) => {
+        $.ajax({
+            url: "/sparrow/common/php/js_stored.php"
+            ,cache: false
+            // ,timeout: time_out
+            ,type:'POST'
+            ,dataType: 'json'
+            ,data:{
+                argArr: this.argArr
+                ,storedName: this.storedName
+            }
+            //,processData: false
+            //,contentType: false
+            //,traditional: true
+        }).then(
+            function(data: any){
+                console.log(data);
+                resolve(data);
+            },
+            function(jqXHR: JQueryXHR, textStatus: string, errorThrown: string){
+                console.log("更新処理に失敗しました");
+                console.log("XMLHttpRequest : " + jqXHR.status);
+                console.log("textStatus     : " + textStatus);
+                console.log("errorThrown    : " + errorThrown);
+                reject(0);
+            }
+        );
+    });
+  }
+}

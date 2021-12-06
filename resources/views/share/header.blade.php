a467da3c0db20aea108388afbe0b9114c9c1721d
osugi <oosugi-t@vehiclevision.jp>
2021-12-06 16:52:10 +0900

    header.blade修正

---
+++ resources/views/share/header.blade.php
@@ -0,0 +1,25 @@
+  <nav class="navbar navbar-expand-lg navbar-light">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#Navber" aria-controls="Navber" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse justify-content-around" id="Navber">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <a class="nav-link home" href="home.html">ホーム</a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link userScore" href="userScore.html">点数確認</a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link ranking" href="ranking.html">順位表</a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link resultList" href="resultList.html" tabindex="-1" aria-disabled="true">結果一覧</a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link rule" href="rule.html" tabindex="-1" aria-disabled="true">ルールDL</a>
+        </li>
+      </ul>
+    </div>
+  </nav>

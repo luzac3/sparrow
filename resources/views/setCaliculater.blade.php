9f458a3af7f7e0a19714387fe65d842b08323ee5
osugi <oosugi-t@vehiclevision.jp>
2021-12-13 14:19:30 +0900

    インデント修正 不要処理を削除

--- resources/views/setCaliculater.blade.php
+++ resources/views/setCaliculater.blade.php
@@ -9,51 +9,55 @@
 <title>セット料金計算ツール</title>
 </head>
 <body>
-<form class="form-signin">
-  <div class="caliculater">
-    <p>
-      入場時間：
-      <input class="inHour" type="number" name="" value="">
-      時　
-      <input class="inMinutes" type="number" name="" value="">
-      分
-    </p>
-    <p>
-      退場時間：
-      <input class="outHour" type="number" name="" value="">
-      時　
-      <input class="outMinutes" type="number" name="" value="">
-      分
-    </p>
-    <p>
-      加算時間単位：
-      <select class="unitOfMinutes" name="">
-        <option value="10">10</option>
-        <option value="10">30</option>
-        <option value="10">60</option>
-      </select>
-      分　
-    </p>
-    <p>
-      金額：
-      <input class="amount" type="number" name="" value="">
-      円　
-    </p>
-    <p>
-      人数：
-      <select class="numberOfPeople" name="">
-        <option value="10">3</option>
-        <option value="10">4</option>
-      </select>
-      人
-    </p>
-    <br>
-    <p class="plice">一人：<span class="showPlice"></span>円</p>
-  </div>
-  <div id="test">
 
+<div class="container">
+  <h1 id="title" class="d-flex align-items-center justify-content-center">セット料金計算ツール</h1>
+  @include('share.header')
+  <div class="main">
+    <form class="form-signin">
+      <div class="caliculater">
+        <p>
+          入場時間：
+          <input class="inHour" type="number" name="" value="">
+          時　
+          <input class="inMinutes" type="number" name="" value="">
+          分
+        </p>
+        <p>
+          退場時間：
+          <input class="outHour" type="number" name="" value="">
+          時　
+          <input class="outMinutes" type="number" name="" value="">
+          分
+        </p>
+        <p>
+          加算時間単位：
+          <select class="unitOfMinutes" name="">
+            <option value="10">10</option>
+            <option value="10">30</option>
+            <option value="10">60</option>
+          </select>
+          分　
+        </p>
+        <p>
+          金額：
+          <input class="amount" type="number" name="" value="">
+          円　
+        </p>
+        <p>
+          人数：
+          <select class="numberOfPeople" name="">
+            <option value="10">3</option>
+            <option value="10">4</option>
+          </select>
+          人
+        </p>
+        <br>
+        <p class="plice">一人：<span class="showPlice"></span>円</p>
+      </div>
+    </form>
   </div>
-</form>
+</div>
 
 @include('share.htmlFooter')
 

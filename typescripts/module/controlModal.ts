db463f3741a1ca6ac75e0463d77101fab19f4844
luzac3 <rey.silvy@gmail.com>
2021-12-14 13:57:45 +0900

    モーダルのファイル名修正

---
+++ typescripts/module/controlModal.ts
@@ -0,0 +1,38 @@
+import * as bootstrap from 'bootstrap';
+
+export class ControlModal {
+  modalObject: HTMLElement;
+  modal: bootstrap.Modal;
+
+  constructor(modalObject: HTMLElement) {
+    this.modalObject = modalObject;
+    this.modal = new bootstrap.Modal(modalObject, {backdrop: true} );
+  }
+
+  openModal(modalBodyMessage: string) {
+    // modalの内容書き換え
+    this.modalObject.getElementsByClassName('modal-body')[0].textContent = modalBodyMessage;
+    this.modal.show();
+    return new Promise((resolve: (value?: Event) => void) => {
+      const listner = resolve;
+      const modalButtons = this.modalObject.getElementsByTagName('button');
+      Array.prototype.forEach.call(modalButtons, modalButton => {
+        modalButton.addEventListener(
+          'click',
+          listner,
+          {once: true}
+        );
+      });
+    });
+  }
+
+  clickOk() {
+    this.modal.hide();
+    return true;
+  }
+
+  clickCancel() {
+    this.modal.hide();
+    return false;
+  }
+}

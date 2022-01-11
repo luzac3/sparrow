69bd06b4610c31a2e5aeed5441b335307b06b41e
osugi <oosugi-t@vehiclevision.jp>
2022-01-11 10:26:00 +0900

    項目のname属性名を修正

--- resources/views/registerEventRule.blade.php
+++ resources/views/registerEventRule.blade.php
@@ -43,44 +43,44 @@
         </tr>
         <tr>
           <th>後付け</th>
-          <td><input type="radio" name="kuitan" value="">あり</td>
-          <td><input type="radio" name="kuitan" value="">なし</td>
+          <td><input type="radio" name="atoduke" value="">あり</td>
+          <td><input type="radio" name="atoduke" value="">なし</td>
         </tr>
         <tr>
           <th>ピンヅモ</th>
-          <td><input type="radio" name="kuitan" value="">あり</td>
-          <td><input type="radio" name="kuitan" value="">なし</td>
+          <td><input type="radio" name="pindumo" value="">あり</td>
+          <td><input type="radio" name="pindumo" value="">なし</td>
         </tr>
         <tr>
           <th>一発</th>
-          <td><input type="radio" name="kuitan" value="">あり</td>
-          <td><input type="radio" name="kuitan" value="">なし</td>
+          <td><input type="radio" name="ippatsu" value="">あり</td>
+          <td><input type="radio" name="ippatsu" value="">なし</td>
         </tr>
         <tr>
           <th>流し満貫</th>
-          <td><input type="radio" name="kuitan" value="">あり</td>
-          <td><input type="radio" name="kuitan" value="">なし</td>
+          <td><input type="radio" name="nagashi" value="">あり</td>
+          <td><input type="radio" name="nagashi" value="">なし</td>
         </tr>
         <tr>
           <th>国士無双時暗槓への槍槓</th>
-          <td><input type="radio" name="kuitan" value="">あり</td>
-          <td><input type="radio" name="kuitan" value="">なし</td>
+          <td><input type="radio" name="kokushi_chankan" value="">あり</td>
+          <td><input type="radio" name="kokushi_chankan" value="">なし</td>
         </tr>
         <tr>
           <th>發なし緑一色</th>
-          <td><input type="radio" name="kuitan" value="">あり</td>
-          <td><input type="radio" name="kuitan" value="">なし</td>
+          <td><input type="radio" name="hatsu_ryuiso" value="">あり</td>
+          <td><input type="radio" name="hatsu_ryuiso" value="">なし</td>
         </tr>
         <tr>
           <th>役満の複合</th>
-          <td><input type="radio" name="kuitan" value="">あり</td>
-          <td><input type="radio" name="kuitan" value="">なし</td>
+          <td><input type="radio" name="fukugou" value="">あり</td>
+          <td><input type="radio" name="fukugou" value="">なし</td>
         </tr>
         <!-- 複合がアリの場合のみ -->
         <tr>
           <th>いくつまで複合するか</th>
           <td rowspan="2">
-            <select class="" name="">
+            <select class="" name="fukugou_su">
               <option value="0">無制限</option>
               <option value="2">2:ダブル役満</option>
               <option value="3">3:トリプル役満</option>
@@ -96,33 +96,45 @@
         </tr>
         <tr>
           <th>四暗刻単騎</th>
-          <td><input type="radio" name="kuitan" value="">あり</td>
-          <td><input type="radio" name="kuitan" value="">なし</td>
+          <td><input type="radio" name="suanko_tanki" value="">あり</td>
+          <td><input type="radio" name="suanko_tanki" value="">なし</td>
         </tr>
         <tr>
           <th>国士無双13面待ち</th>
-          <td><input type="radio" name="kuitan" value="">あり</td>
-          <td><input type="radio" name="kuitan" value="">なし</td>
+          <td><input type="radio" name="kokushi_13men" value="">あり</td>
+          <td><input type="radio" name="kokushi_13men" value="">なし</td>
         </tr>
         <tr>
           <th>純正九蓮宝塔</th>
-          <td><input type="radio" name="kuitan" value="">あり</td>
-          <td><input type="radio" name="kuitan" value="">なし</td>
+          <td><input type="radio" name="junsei_churen" value="">あり</td>
+          <td><input type="radio" name="junsei_churen" value="">なし</td>
         </tr>
         <tr>
           <th>發なし緑一色</th>
-          <td><input type="radio" name="kuitan" value="">あり</td>
-          <td><input type="radio" name="kuitan" value="">なし</td>
+          <td><input type="radio" name="tatsunashi_ryuiso_double" value="">あり</td>
+          <td><input type="radio" name="tatsunashi_ryuiso_double" value="">なし</td>
         </tr>
         <tr>
           <th>大四喜</th>
-          <td><input type="radio" name="kuitan" value="">あり</td>
-          <td><input type="radio" name="kuitan" value="">なし</td>
+          <td><input type="radio" name="daisushi" value="">あり</td>
+          <td><input type="radio" name="daisushi" value="">なし</td>
         </tr>
         <tr>
           <th>四槓子</th>
-          <td><input type="radio" name="kuitan" value="">あり</td>
-          <td><input type="radio" name="kuitan" value="">なし</td>
+          <td><input type="radio" name="sukantsu" value="">あり</td>
+          <td><input type="radio" name="sukantsu" value="">なし</td>
+        </tr>
+      </tbody>
+      <thead>
+        <tr rowspan="3">
+          縛り
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th>5巡目以降二翻縛り</th>
+          <td><input type="radio" name="ryanhan" value="">あり</td>
+          <td><input type="radio" name="ryanhan" value="">なし</td>
         </tr>
       </tbody>
       <thead>
@@ -133,28 +145,28 @@
       <tbody>
         <tr>
           <th>四風連打</th>
-          <td><input type="radio" name="kuitan" value="">流局</td>
-          <td><input type="radio" name="kuitan" value="">続行</td>
+          <td><input type="radio" name="sufurenda" value="">流局</td>
+          <td><input type="radio" name="sufurenda" value="">続行</td>
         </tr>
         <tr>
           <th>九種九牌</th>
-          <td><input type="radio" name="kuitan" value="">流局</td>
-          <td><input type="radio" name="kuitan" value="">続行</td>
+          <td><input type="radio" name="kyushukyuhai" value="">流局</td>
+          <td><input type="radio" name="kyushukyuhai" value="">続行</td>
         </tr>
         <tr>
           <th>三家和</th>
-          <td><input type="radio" name="kuitan" value="">流局</td>
-          <td><input type="radio" name="kuitan" value="">続行</td>
+          <td><input type="radio" name="sanchaho" value="">流局</td>
+          <td><input type="radio" name="sanchaho" value="">続行</td>
         </tr>
         <tr>
           <th>四家和</th>
-          <td><input type="radio" name="kuitan" value="">流局</td>
-          <td><input type="radio" name="kuitan" value="">続行</td>
+          <td><input type="radio" name="suchaho" value="">流局</td>
+          <td><input type="radio" name="suchaho" value="">続行</td>
         </tr>
         <tr>
           <th>四開槓</th>
-          <td><input type="radio" name="kuitan" value="">流局</td>
-          <td><input type="radio" name="kuitan" value="">続行</td>
+          <td><input type="radio" name="sukaikan" value="">流局</td>
+          <td><input type="radio" name="sukaikan" value="">続行</td>
         </tr>
       </tbody>
       <thead>
@@ -165,18 +177,18 @@
       <tbody>
         <tr>
           <th>裏ドラ</th>
-          <td><input type="radio" name="kuitan" value="">あり</td>
-          <td><input type="radio" name="kuitan" value="">なし</td>
+          <td><input type="radio" name="uradora" value="">あり</td>
+          <td><input type="radio" name="uradora" value="">なし</td>
         </tr>
         <tr>
           <th>槓ドラ(暗槓)</th>
-          <td><input type="radio" name="kuitan" value="">即めくり</td>
-          <td><input type="radio" name="kuitan" value="">後めくり</td>
+          <td><input type="radio" name="kandora_ankan" value="">即めくり</td>
+          <td><input type="radio" name="kandora_ankan" value="">後めくり</td>
         </tr>
         <tr>
           <th>槓ドラ(明槓)</th>
-          <td><input type="radio" name="kuitan" value="">即めくり</td>
-          <td><input type="radio" name="kuitan" value="">後めくり</td>
+          <td><input type="radio" name="kandora_minkan" value="">即めくり</td>
+          <td><input type="radio" name="kandora_minkan" value="">後めくり</td>
         </tr>
       </tbody>
       <thead>
@@ -187,18 +199,18 @@
       <tbody>
         <tr>
           <th>大三元</th>
-          <td><input type="radio" name="kuitan" value="">責任払い</td>
-          <td><input type="radio" name="kuitan" value="">なし</td>
+          <td><input type="radio" name="daisangen" value="">責任払い</td>
+          <td><input type="radio" name="daisangen" value="">なし</td>
         </tr>
         <tr>
           <th>四喜和</th>
-          <td><input type="radio" name="kuitan" value="">責任払い</td>
-          <td><input type="radio" name="kuitan" value="">なし</td>
+          <td><input type="radio" name="sushiho" value="">責任払い</td>
+          <td><input type="radio" name="sushiho" value="">なし</td>
         </tr>
         <tr>
           <th>大明槓嶺上開花</th>
-          <td><input type="radio" name="kuitan" value="">責任払い</td>
-          <td><input type="radio" name="kuitan" value="">なし</td>
+          <td><input type="radio" name="daiminkan_rinshan" value="">責任払い</td>
+          <td><input type="radio" name="daiminkan_rinshan" value="">なし</td>
         </tr>
       </tbody>
       <thead>
@@ -209,29 +221,29 @@
       <tbody>
         <tr>
           <th>永田町ルール</th>
-          <td><input type="radio" name="kuitan" value="">あり</td>
-          <td><input type="radio" name="kuitan" value="">なし</td>
+          <td><input type="radio" name="nagatacho" value="">あり</td>
+          <td><input type="radio" name="nagatacho" value="">なし</td>
         </tr>
         <tr>
           <th>割れ目</th>
-          <td><input type="radio" name="kuitan" value="">あり</td>
-          <td><input type="radio" name="kuitan" value="">なし</td>
+          <td><input type="radio" name="wareme" value="">あり</td>
+          <td><input type="radio" name="wareme" value="">なし</td>
         </tr>
         <tr>
           <th>青天井</th>
-          <td><input type="radio" name="kuitan" value="">あり</td>
-          <td><input type="radio" name="kuitan" value="">なし</td>
+          <td><input type="radio" name="aotenjo" value="">あり</td>
+          <td><input type="radio" name="aotenjo" value="">なし</td>
         </tr>
         <tr>
           <th>同時和了</th>
-          <td><input type="radio" name="kuitan" value="">あり</td>
-          <td><input type="radio" name="kuitan" value="">なし</td>
+          <td><input type="radio" name="doujihora" value="">あり</td>
+          <td><input type="radio" name="doujihora" value="">なし</td>
         </tr>
         <!-- 同時和了がアリの場合のみ -->
         <tr>
           <th>同時に和了できる人数</th>
           <td rowspan="2">
-            <select class="" name="">
+            <select class="" name="doujihora_num">
               <option value="2">2人</option>
               <option value="3">3人</option>
             </select>
@@ -246,103 +258,64 @@
       <tbody>
         <tr>
           <th>オープンリーチ</th>
-          <td><input type="radio" name="kuitan" value="">あり</td>
-          <td><input type="radio" name="kuitan" value="">なし</td>
+          <td><input type="radio" name="open" value="">あり</td>
+          <td><input type="radio" name="open" value="">なし</td>
         </tr>
         <tr>
           <th>三連刻</th>
-          <td><input type="radio" name="kuitan" value="">あり</td>
-          <td><input type="radio" name="kuitan" value="">なし</td>
+          <td><input type="radio" name="sanrenko" value="">あり</td>
+          <td><input type="radio" name="sanrenko" value="">なし</td>
         </tr>
         <tr>
           <th>一色三順</th>
-          <td><input type="radio" name="kuitan" value="">あり</td>
-          <td><input type="radio" name="kuitan" value="">なし</td>
+          <td><input type="radio" name="isu_sanjun" value="">あり</td>
+          <td><input type="radio" name="isu_sanjun" value="">なし</td>
         </tr>
         <tr>
           <th>パンダ</th>
-          <td><input type="radio" name="kuitan" value="">あり</td>
-          <td><input type="radio" name="kuitan" value="">なし</td>
+          <td><input type="radio" name="panda" value="">あり</td>
+          <td><input type="radio" name="panda" value="">なし</td>
         </tr>
         <tr>
           <th>大車輪</th>
-          <td><input type="radio" name="kuitan" value="">あり</td>
-          <td><input type="radio" name="kuitan" value="">なし</td>
+          <td><input type="radio" name="daisharin" value="">あり</td>
+          <td><input type="radio" name="daisharin" value="">なし</td>
         </tr>
         <tr>
           <th>四連刻</th>
-          <td><input type="radio" name="kuitan" value="">あり</td>
-          <td><input type="radio" name="kuitan" value="">なし</td>
+          <td><input type="radio" name="surenko" value="">あり</td>
+          <td><input type="radio" name="surenko" value="">なし</td>
         </tr>
         <tr>
           <th>八連荘</th>
-          <td><input type="radio" name="kuitan" value="">あり</td>
-          <td><input type="radio" name="kuitan" value="">なし</td>
+          <td><input type="radio" name="parenchan" value="">あり</td>
+          <td><input type="radio" name="parenchan" value="">なし</td>
         </tr>
         <tr>
           <th>人和</th>
-          <td><input type="radio" name="kuitan" value="">あり</td>
-          <td><input type="radio" name="kuitan" value="">なし</td>
+          <td><input type="radio" name="renho" value="">あり</td>
+          <td><input type="radio" name="renho" value="">なし</td>
         </tr>
       </tbody>
       <thead>
         <tr rowspan="3">
           ローカル役2
+          <!-- ローカル役追加できるようにしても良いかも -->
         </tr>
       </thead>
       <tbody>
         <tr>
-          <th>オープンリーチ</th>
-          <td><input type="radio" name="kuitan" value="">あり</td>
-          <td><input type="radio" name="kuitan" value="">なし</td>
-        </tr>
-        <tr>
-          <th>三連刻</th>
-          <td><input type="radio" name="kuitan" value="">あり</td>
-          <td><input type="radio" name="kuitan" value="">なし</td>
-        </tr>
-        <tr>
-          <th>一色三順</th>
-          <td><input type="radio" name="kuitan" value="">あり</td>
-          <td><input type="radio" name="kuitan" value="">なし</td>
-        </tr>
-        <tr>
-          <th>パンダ</th>
-          <td><input type="radio" name="kuitan" value="">あり</td>
-          <td><input type="radio" name="kuitan" value="">なし</td>
-        </tr>
-        <tr>
-          <th>大車輪</th>
-          <td><input type="radio" name="kuitan" value="">あり</td>
-          <td><input type="radio" name="kuitan" value="">なし</td>
-        </tr>
-        <tr>
-          <th>四連刻</th>
-          <td><input type="radio" name="kuitan" value="">あり</td>
-          <td><input type="radio" name="kuitan" value="">なし</td>
-        </tr>
-        <tr>
-          <th>八連荘</th>
-          <td><input type="radio" name="kuitan" value="">あり</td>
-          <td><input type="radio" name="kuitan" value="">なし</td>
-        </tr>
-        <tr>
-          <th>人和</th>
+          <th>サンプル</th>
           <td><input type="radio" name="kuitan" value="">あり</td>
           <td><input type="radio" name="kuitan" value="">なし</td>
         </tr>
       </tbody>
     </table>
   </div>
-  <p>
-    <input class="user_name" type="text" name="" value="">ユーザ名/
-    <input class="user_id" type="text" name="" value="">ツイッターID
-  </p>
-  <p><button class="user_add" type="button" name="button">追加</button></p>
   <p><button class="user_add" type="button" name="button">登録</button></p>
 
   <!-- このボタン、PHP経由で表示すべきか。ページをロードして表示するならその方がよさそう -->
-  <button type="button" name="button">ルール登録へ</button>
+  <button type="button" name="button">確認画面へ</button>
 
   <input type="text" name="" value="">
 </form>
